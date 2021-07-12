d649f782df8bf8bee7686f021aabda3bcb3d78d2
Vignesh-2896 <vigneshb2896@gmail.com>
2021-07-12 20:21:26 +0530

    Linting.

--- index.js
+++ index.js
@@ -1,77 +1,95 @@
-const futureForecast = document.querySelector("#futureForecast");
 const todayForecast = document.querySelector("#todayForecast");
 
 document.addEventListener("keydown",(event) => {
     if(event.key == "Enter") validateCity();
-})
+});
 
 const fetchWeatherData = async(cityName) => {
     try {
-    let fetchedData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${Your_App_Key_Here}&units=metric`,{mode:'cors'});    
-    let currentDayData = await fetchedData.json();
-    clearData();
-
-    h1 = document.createElement("h1");
-    h1.textContent = currentDayData.name+", "+currentDayData.sys.country;
-    h1.style.textAlign = "center";
-    todayForecast.appendChild(h1);
-
-    populateFutureForecast(currentDayData.coord.lat, currentDayData.coord.lon);
-    } catch {
+        let fetchedData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${Your_App_Key_Here}&units=metric`,{mode:'cors'});    
+        let currentDayData = await fetchedData.json();
+        clearData();
+        document.querySelector("#todayForecast_City").textContent = currentDayData.name+", "+currentDayData.sys.country;
+        populateFutureForecast(currentDayData.coord.lat, currentDayData.coord.lon);
+    } catch(error) {
         alert("Oops! Incorrect city name entered. Please try again.");
+        console.log(error);
     }
-}
+};
 
 const clearData = () => {
-    while(todayForecast.firstChild) todayForecast.removeChild(todayForecast.firstChild)
-    while(futureForecast.firstChild) futureForecast.removeChild(futureForecast.firstChild)
+    let tableTodayForecast = document.querySelector("#todayForecast_Table");
+    while(tableTodayForecast.firstChild) tableTodayForecast.removeChild(tableTodayForecast.firstChild);
+
+    let futureTableForecast = document.querySelector("#futureForecast_Table");
+    while(futureTableForecast.firstChild) futureTableForecast.removeChild(futureTableForecast.firstChild);
+
     todayForecast.style.borderBottom = "";
-}
+    document.querySelector("#todayForecast_City").textContent = "";
+    document.querySelector("#todayForecast_Date").textContent = "";
+    document.querySelector("#futureForecast_Heading").textContent = "";
+};
 
 const drillAndFetchData = (data,attribute) => {
-    let fieldData = "";
-    if(attribute == "Date"){
-        fieldData = new Date(data.dt * 1000).toLocaleString("en-us",{month:"long",day:"numeric",year:"numeric"});
-    } else if(attribute == "Temperature"){
-        fieldData = (data.temp.max).toFixed(1)+"°/"+(data.temp.min).toFixed(1)+"°";
-    } else if(attribute == "Weather Type"){
-        fieldData = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-    } else if(attribute == "Humidity"){
-        fieldData = data.humidity+"%";
-    } else if (attribute == "Possiblity of Rain"){
-        fieldData = (data.pop * 100).toFixed(1)+"%";
-    } else if (attribute == "Weather Conditions"){
-        fieldData = data.weather[0].description;
-    } else if (attribute == "Sunrise"){
-        fieldData = new Date(data.sunrise * 1000).toLocaleString("en-us",{hour:"numeric",minute:"numeric"});
-    } else if (attribute == "Sunset"){
-        fieldData = new Date(data.sunset * 1000).toLocaleString("en-us",{hour:"numeric",minute:"numeric"});
+
+    const weatherOptions = {
+        "Date": _ => transformDate(data.dt, "Date"),
+        "Temperature": _ => fetchTemperature(data),
+        "Weather Type": _ => fetchWeatherIcon(data),
+        "Possiblity of Rain": _ => fetchRainPossiblity(data),
+        "Weather Conditions": _ => fetchWeatherConditions(data),
+        "Humidity": _ => fetchHumidity(data),
+        "Sunrise": _ => transformDate(data.sunrise, "Time"),
+        "Sunset": _ => transformDate(data.sunset, "Time"),
+    };
+    if(weatherOptions.hasOwnProperty(attribute)) return weatherOptions[attribute]();
+};
+
+const transformDate = (data, requiredDateType) => {
+    if(requiredDateType){
+        return new Date(data * 1000).toLocaleString("en-us",{month:"long",day:"numeric",year:"numeric"});
+    } else {
+        return new Date(data * 1000).toLocaleString("en-us",{hour:"numeric",minute:"numeric"});
     }
-    return fieldData;
-}
+};
+
+const fetchTemperature = (data) => {
+    return (data.temp.max).toFixed(1)+"°/"+(data.temp.min).toFixed(1)+"°";
+};
+
+const fetchHumidity = (data) => {
+    return data.humidity+"%";
+};
+
+const fetchWeatherIcon = (data) => {
+    return `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+};
+
+const fetchRainPossiblity = (data) => {
+    return (data.pop * 100).toFixed(1)+"%";
+};
+
+const fetchWeatherConditions = (data) => {
+    return data.weather[0].description;
+};
 
 const populateFutureForecast = async(latitutde, longitude) => {
 
-    fetchedData = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitutde}&lon=${longitude}&appid=${Your_App_Key_Here}&exclude=current,minutely,hourly&units=Metric`,{mode:'cors'});
+    let fetchedData = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitutde}&lon=${longitude}&appid=${Your_App_Key_Here}&exclude=current,minutely,hourly&units=Metric`,{mode:'cors'});
     let futureDaysData = await fetchedData.json();
     futureDaysData = futureDaysData.daily;
     populateTodayForecast(futureDaysData[0]);
     futureDaysData.shift();
 
-    const h2 = document.createElement("h2");
-    h2.style.textAlign = "center";
-    h2.textContent = "Forecast for next week"
-    futureForecast.appendChild(h2);
+    document.querySelector("#futureForecast_Heading").textContent = "Forecast for next week";
 
-    const table = document.createElement("table");
-    table.setAttribute("border", 1);
-    table.style.marginBottom = "10px";
+    const table = document.querySelector("#futureForecast_Table");
 
-    const requiredWeatherProperties = ["Date", "Weather Type","Temperature","Humidity","Weather Conditions","Possiblity of Rain","Sunrise","Sunset"]
+    const requiredWeatherProperties = ["Date", "Weather Type","Temperature","Humidity","Weather Conditions","Possiblity of Rain","Sunrise","Sunset"];
     requiredWeatherProperties.forEach(function(weatherProperty){
         const tr = document.createElement("tr");
         tr.style.textAlign = "center";
-        counter = 0;
+        let counter = 0;
         futureDaysData.forEach(function(dailyData){
             if(counter == 0){
                 const th = document.createElement("th");
@@ -91,40 +109,37 @@ const populateFutureForecast = async(latitutde, longitude) => {
         });
         table.appendChild(tr);
     });
-    futureForecast.appendChild(table);
-}
+};
 
 const populateTodayForecast = (data) => {
 
-    h2 = document.createElement("h2");
-    h2.textContent = "Today's Forecast - "+drillAndFetchData(data,"Date");
-    todayForecast.appendChild(h2);
+    document.querySelector("#todayForecast_Date").textContent = "Today's Forecast - "+drillAndFetchData(data,"Date");
 
-    const img = document.createElement("img");
-    img.src = drillAndFetchData(data,"Weather Type");
-    todayForecast.appendChild(img);
+    document.querySelector("#todayForecast_Type").src = drillAndFetchData(data,"Weather Type");
 
-    const table = document.createElement("table");
-    table.setAttribute("border", 1);
-    table.style.marginBottom = "10px";
+    const table = document.querySelector("#todayForecast_Table");
 
-    const requiredWeatherProperties = ["Temperature","Humidity","Weather Conditions","Possiblity of Rain","Sunrise","Sunset"]
+    const requiredWeatherProperties = ["Temperature","Humidity","Weather Conditions","Possiblity of Rain","Sunrise","Sunset"];
     requiredWeatherProperties.forEach(function(weatherProperty){
         const tr = document.createElement("tr");
 
         const th = document.createElement("th");
         th.textContent = weatherProperty;
-        tr.appendChild(th)
+        tr.appendChild(th);
 
         const td = document.createElement("td");
-        fetchedData = drillAndFetchData(data, weatherProperty)
+        let fetchedData = drillAndFetchData(data, weatherProperty);
         td.textContent = fetchedData ;
         tr.appendChild(td);
 
         table.appendChild(tr);
-    })
-    todayForecast.appendChild(table);
+    });
     todayForecast.style.borderBottom = "1px solid black";
+};
+
+
+const populateData = (table, data) => {
+    
 }
 
 const validateCity = () => {
@@ -135,4 +150,4 @@ const validateCity = () => {
         document.getElementById("city_name").value = "";
         fetchWeatherData(city);
     } else alert("Oops! Enter a City First.");
-}
+};
